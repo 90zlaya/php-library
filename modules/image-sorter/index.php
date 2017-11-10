f4804a20b360155922489ee926762d97b0b01e5c
90zlaya <contact@zlatanstajic.com>
2017-11-10 16:18:26 +0100

    image-sorter report added

--- modules/image-sorter/index.php
+++ modules/image-sorter/index.php
@@ -17,6 +17,35 @@ ini_set('max_execution_time', 3600); // 3600 s = 1 h
 $where_to_read_files = 'D:/Browser/slikeVelike/';
 $where_to_make_dir = 'D:/Browser/CDN/';
 $folder_sufix = '000';
+$number_of_directories = 1000;
+
+// -----------------------------------------------------------------------------
+
+/**
+* Report array
+*/
+$image_sorter = array(
+    'folders' => array(
+        'number' => array(
+            'created'     => 0,
+            'not_created' => 0,
+        ),
+        'report' => array(
+            'created'     => array(),
+            'not_created' => array(),
+        ),
+    ),
+    'files' => array(
+        'number' => array(
+            'copied'     => 0,
+            'not_copied' => 0,
+        ),
+        'report' => array(
+            'copied'     => array(),
+            'not_copied' => array(),
+        ),
+    ),
+);
 
 // -----------------------------------------------------------------------------
 
@@ -36,7 +65,7 @@ phplibrary\Format::pre($listing, FALSE);
 /**
 * Create directories
 */
-for($i=0; $i<501; $i++)
+for($i=0; $i<$number_of_directories; $i++)
 {
     $i_length = strlen($i);
     
@@ -65,11 +94,17 @@ for($i=0; $i<501; $i++)
     
     if(!file_exists($new_folder))
     {
-        $is_created = mkdir($new_folder);
+        $is_created = @mkdir($new_folder);
         
-        if(!$is_created)
+        if($is_created)
         {
-            echo 'Not created: ' . $new_folder . '<br/>';
+            $image_sorter['folders']['number']['created']++;
+            array_push($image_sorter['folders']['report']['created'], $new_folder);
+        }
+        else
+        {
+            $image_sorter['folders']['number']['not_created']++;
+            array_push($image_sorter['folders']['report']['not_created'], $new_folder);
         }
     }
 }
@@ -89,11 +124,23 @@ foreach($listing['listing'] as $item)
     
     $is_copied = @copy($location_from, $location_to);
     
-    if(!$is_copied)
+    if($is_copied)
     {
-        echo 'Not copied: ' . $file . '<br/>';
+        $image_sorter['files']['number']['copied']++;
+        array_push($image_sorter['files']['report']['copied'], $file);
+    }
+    else
+    {
+        $image_sorter['files']['number']['not_copied']++;
+        array_push($image_sorter['files']['report']['not_copied'], $file);
     }
 }
 
 // -----------------------------------------------------------------------------
+
+echo 'Folders created/not created: ' . $image_sorter['folders']['number']['created'] . '/' . $image_sorter['folders']['number']['not_created'] . '<br/>';
+echo 'Files copied/not copied: ' . $image_sorter['files']['number']['copied'] . '/' . $image_sorter['files']['number']['not_copied'] . '<br/>';
+
+phplibrary\Format::pre($image_sorter);
+
 ?>
