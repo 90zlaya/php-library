867d541be159041b3557de14fde59cd207e8cb71
90zlaya <contact@zlatanstajic.com>
2018-01-23 14:25:33 +0100

    Removed pages demo.php and log.php and replaced with view.php

--- log.php
+++
@@ -1,9 +0,0 @@
-<?php
-    $fh = fopen('log.txt','r');
-    
-    while ($line = fgets($fh))
-    {
-        echo $line . '<br>';
-    }
-    fclose($fh);
-?>
--- view.php
+++ view.php
@@ -33,7 +33,40 @@
         }
         else
         {
-            header('Location: ' . $home_page);
+            switch ($location)
+            {
+                case 'demo': 
+                {
+                    echo '<h2/>';
+                    echo ucfirst($php_library['folders']['demonstrations']);
+                    echo '</h2>' . PHP_EOL;
+                    echo '<ol>'; 
+                        foreach ($php_library['list']['classes'] as $item)
+                        {
+                            echo '<li>';
+                            echo '<a href="view.php?page=' . $php_library['folders']['demonstrations'] . '/' . $item['name'] . '">'; 
+                            echo $item['name'];
+                            echo '</a>';
+                            echo '</li>' . PHP_EOL;
+                        }
+                    echo '</ol>' . PHP_EOL;
+                    
+                    echo '<h2/>';
+                } break;
+                
+                case 'log':
+                {
+                    $fh = fopen('log.txt','r');
+        
+                    while ($line = fgets($fh))
+                    {
+                        echo $line . '<br>';
+                    }
+                    fclose($fh);
+                } break;
+                
+                default: header('Location: ' . $home_page);
+            }
         }
     }
 ?>
