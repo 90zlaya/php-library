7ea414467b3ab190456f94c4a092ac760321d9b3
90zlaya <contact@zlatanstajic.com>
2018-03-06 08:56:03 +0100

    Completed unit testing

---
+++ tests/Directory_Lister_Test.php
@@ -0,0 +1,202 @@
+<?php
+/**
+* Directory_Lister
+*
+* Directory content retrieval
+*
+* @package      PHP Library
+* @subpackage   phplibrary
+* @category     Files
+* @author       Zlatan Stajić <contact@zlatanstajic.com>
+*/
+use PHPUnit\Framework\TestCase as Test_Case;
+use phplibrary\Directory_Lister as directory_lister;
+
+/**
+* Testing Directory_Lister class
+*/
+class Directory_Lister_Test extends Test_Case {
+    
+    // -------------------------------------------------------------------------
+    
+    /**
+    * Directory for testing Directory_Lister class
+    * 
+    * @var String
+    */
+    private $directory = 'outsource/directory_lister/';
+    
+    // -------------------------------------------------------------------------
+    
+    /**
+    * Minimalistic input to listing method
+    */
+    public function test_lister_on_minimalistic_input()
+    {
+        $listing = directory_lister::listing(array(
+            'directory'  => realpath($this->directory) . DIRECTORY_SEPARATOR,
+            'method'     => 'crawl',
+        ));
+        
+        $this->assertDirectoryExists($this->directory);
+        $this->assertDirectoryIsReadable($this->directory);
+        $this->assertInternalType('array', $listing);
+        $this->assertArrayHasKey('listing', $listing);
+        $this->assertArrayHasKey('count', $listing);
+        $this->assertArrayHasKey('max', $listing);
+        $this->assertEquals($listing['count'], $listing['max']);
+    }
+    
+    // -------------------------------------------------------------------------
+    
+    /**
+    * Included years parameter to listing method
+    */
+    public function test_lister_on_years_input()
+    {
+        $listing = directory_lister::listing(array(
+            'directory'  => realpath($this->directory) . DIRECTORY_SEPARATOR,
+            'method'     => 'crawl',
+            'years'      => array(
+                2013,
+                2016,
+                2017,
+            ),
+        ));
+        
+        $this->assertEquals(25, $listing['count']);
+        $this->assertEquals(25, $listing['max']);
+    }
+    
+    // -------------------------------------------------------------------------
+    
+    /**
+    * Included types parameter to listing method
+    */
+    public function test_lister_on_types_input()
+    {
+        $listing = directory_lister::listing(array(
+            'directory'  => realpath($this->directory) . DIRECTORY_SEPARATOR,
+            'method'     => 'crawl',
+            'years'      => array(
+                2013,
+                2016,
+                2017,
+            ),
+            'types'      => array(
+                'png',
+            ),
+        ));
+        
+        $this->assertEquals(25, $listing['count']);
+        $this->assertEquals(25, $listing['max']);
+    }
+    
+    // -------------------------------------------------------------------------
+    
+    /**
+    * Included date_start parameter to listing method
+    */
+    public function test_lister_on_date_start_input()
+    {
+        $listing = directory_lister::listing(array(
+            'directory'  => realpath($this->directory) . DIRECTORY_SEPARATOR,
+            'method'     => 'crawl',
+            'years'      => array(
+                2013,
+                2016,
+                2017,
+            ),
+            'types'      => array(
+                'png',
+            ),
+            'date_start' => '01-12',
+        ));
+        
+        $this->assertEquals(4, $listing['count']);
+        $this->assertEquals(25, $listing['max']);
+    }
+    
+    // -------------------------------------------------------------------------
+    
+    /**
+    * Included date_end parameter to listing method
+    */
+    public function test_lister_on_date_end_input()
+    {
+        $listing = directory_lister::listing(array(
+            'directory'  => realpath($this->directory) . DIRECTORY_SEPARATOR,
+            'method'     => 'crawl',
+            'years'      => array(
+                2013,
+                2016,
+                2017,
+            ),
+            'types'      => array(
+                'png',
+            ),
+            'date_start' => '01-12',
+            'date_end'   => '01-14',
+        ));
+        
+        $this->assertEquals(11, $listing['count']);
+        $this->assertEquals(25, $listing['max']);
+    }
+    
+    // -------------------------------------------------------------------------
+    
+    /**
+    * Included delimiter parameter to listing method
+    */
+    public function test_lister_on_delimiter_input()
+    {
+        $listing = directory_lister::listing(array(
+            'directory'  => realpath($this->directory) . DIRECTORY_SEPARATOR,
+            'method'     => 'crawl',
+            'years'      => array(
+                2013,
+                2016,
+                2017,
+            ),
+            'types'      => array(
+                'png',
+            ),
+            'date_start' => '01-12',
+            'date_end'   => '01-14',
+            'delimiter'  => 'mysql',
+        ));
+        
+        $this->assertEquals(1, $listing['count']);
+        $this->assertEquals(25, $listing['max']);
+    }
+    
+    // -------------------------------------------------------------------------
+    
+    /**
+    * Included reverse parameter to listing method
+    */
+    public function test_lister_on_reverse_input()
+    {
+        $listing = directory_lister::listing(array(
+            'directory'  => realpath($this->directory) . DIRECTORY_SEPARATOR,
+            'method'     => 'crawl',
+            'years'      => array(
+                2013,
+                2016,
+                2017,
+            ),
+            'types'      => array(
+                'png',
+            ),
+            'date_start' => '01-12',
+            'date_end'   => '01-14',
+            'delimiter'  => 'mysql',
+            'reverse'    => TRUE,
+        ));
+        
+        $this->assertEquals(10, $listing['count']);
+        $this->assertEquals(25, $listing['max']);
+    }
+    
+    // -------------------------------------------------------------------------
+}
---
+++ tests/File_Test.php
@@ -0,0 +1,123 @@
+<?php
+/**
+* File
+*
+* File-related operations
+*
+* @package      PHP Library
+* @subpackage   phplibrary
+* @category     Files
+* @author       Zlatan Stajić <contact@zlatanstajic.com>
+*/
+use PHPUnit\Framework\TestCase as Test_Case;
+use phplibrary\File as file;
+
+/**
+* Testing File class
+*/
+class File_Test extends Test_Case {
+    
+    // -------------------------------------------------------------------------
+    
+    /**
+    * File parameters
+    * 
+    * @var Array
+    */
+    private $file = array();
+    
+    // -------------------------------------------------------------------------
+    
+    /**
+    * Line to write to file
+    * 
+    * @var String
+    */
+    private $line = 'Test';
+    
+    // -------------------------------------------------------------------------
+    
+    /**
+    * File_Test setup method
+    */
+    public function setUp()
+    {
+        $this->file = array(
+            'location' => realpath('outsource/file/') . DIRECTORY_SEPARATOR,
+            'name'     => 'file.txt',
+        );
+        
+        $this->assertDirectoryIsReadable($this->file['location']);
+        $this->assertDirectoryIsWritable($this->file['location']);
+    }
+    
+    // -------------------------------------------------------------------------
+    
+    /**
+    * Testing write_to_file method
+    */
+    public function test_write_to_file_method()
+    {
+        $this->assertNull(file::write_to_file(
+            $this->file['location'] . $this->file['name'],
+            $this->line
+        ));
+        
+        $this->assertFalse(file::write_to_file('', ''));
+    }
+    
+    // -------------------------------------------------------------------------
+    
+    /**
+    * Testing read_from_file method
+    */
+    public function test_read_from_file_method()
+    {
+        $result = file::read_from_file(
+            $this->file['location'] . $this->file['name']
+        );
+        
+        $this->assertEquals($this->line, $result);
+        
+        $result = file::read_from_file('');
+        
+        $this->assertFalse($result);
+    }
+    
+    // -------------------------------------------------------------------------
+    
+    /**
+    * Testing read_file_contents method
+    */
+    public function test_read_file_contents_method()
+    {
+        $result = file::read_file_contents(
+            $this->file['location'] . $this->file['name']
+        );
+        
+        $this->assertInternalType('array', $result);
+        $this->assertArrayHasKey('status', $result);
+        $this->assertArrayHasKey('items', $result);
+        $this->assertTrue($result['status']);
+        
+        foreach ($result['items'] as $item)
+        {
+            $this->assertEquals($this->line . "\r\n", $item[0]);
+        }
+    }
+    
+    // -------------------------------------------------------------------------
+    
+    /**
+    * Testing force_download method
+    */
+    public function test_force_download_method()
+    {
+        $this->assertNull(file::force_download(
+            'php-library.jpg',
+            'https://link.zlatanstajic.com/images/portfolio/'
+        ));
+    }
+    
+    // -------------------------------------------------------------------------
+}
